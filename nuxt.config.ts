757bdbea7e2cb1d8f4318552eca75de963339459
Rachel Opperman <oppermanre17@gmail.com>
2022-12-17 16:48:52 -0500

    Adds favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
 export default defineNuxtConfig({
   ssr: false,
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'theme-color', content: '#030305' },
+        { name: 'msapplication-TileColor', content: '#030305' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+      link: [
+        { rel: 'manifest', href: '/favicon/site.webmanifest' },
+        { rel: 'mask-icon', href: '/favicon/safari-pinned-tab.svg', color: '#030305' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16x', href: '/favicon/favicon-16x16.png' },
+      ],
+    },
+  },
   modules: ['@nuxtjs/tailwindcss'],
 })
