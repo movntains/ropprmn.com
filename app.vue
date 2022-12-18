26ef1b3d19a3b91cc29dbe0004387988359bedc2
Rachel Opperman <oppermanre17@gmail.com>
2022-12-17 21:25:15 -0500

    Adds error page

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import '@fontsource/jost/300.css'
+import '@fontsource/jost/400.css'
+import '@fontsource/jost/600.css'
+import '@fontsource/jost/700.css'
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -15,10 +15,3 @@
     <AppFooter />
   </div>
 </template>
-
-<script setup lang="ts">
-import '@fontsource/jost/300.css'
-import '@fontsource/jost/400.css'
-import '@fontsource/jost/600.css'
-import '@fontsource/jost/700.css'
-</script>
